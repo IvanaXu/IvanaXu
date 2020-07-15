367c88109725c24f8123016290a1f71c9841c58e
ivan <1440420407@qq.com>
2020-07-15 17:07:57 +0800

    update code 20200715170756

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 import json
 import random
-with open("poetry2.json", "r", encoding="utf-8") as f:
+with open(f"poetry{random.randint(0,2)}.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 title = "# བཀྲ་ཤིས་བདེ་ལེགས་"
