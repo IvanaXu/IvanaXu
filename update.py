c802502fe6f95606739f4d2fdd6e5e3428e9d79d
ivan <1440420407@qq.com>
2020-07-15 16:32:30 +0800

    update code 20200715163230

---
+++ update.py
@@ -0,0 +1,16 @@
+"""
+Thx https://github.com/chinese-poetry/chinese-poetry.
+"""
+import json
+import random
+with open("qianjiashi.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+title = "# བཀྲ་ཤིས་བདེ་ལེགས་"
+ldata = [jdata for idata in data["content"] for jdata in idata["content"]]
+rdata = lambda d: f"{d['chapter']}\n{d['author']}\n" + "\n".join(d['paragraphs'])
+conts = rdata(random.choice(ldata))
+print(conts)
+
+with open("README.md", "w") as f:
+    f.write(f"{title}\n{conts}")
---
+++ update.sh
@@ -0,0 +1,2 @@
+python update.py
+git add *;git commit -m "update code $(date "+%Y%m%d%H%M%S")";git push;
