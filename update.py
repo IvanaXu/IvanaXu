d09ef8598651b211fe3206e9ed5b8d648e510a77
ivan <1440420407@qq.com>
2020-07-16 02:17:25 +0800

    update code 20200716021725

--- update.py
+++ update.py
@@ -7,7 +7,7 @@ title = "# བཀྲ་ཤིས་བདེ་ལེགས་"
 ldata = [jdata for idata in data["content"] for jdata in idata["content"]]
 rdata = lambda d: f"> {d['chapter']}\n> \n> {d['author']}\n> \n> " + "\n> \n> ".join(d['paragraphs']) + "\n>"
 conts = rdata(random.choice(ldata))
-print(ldata, conts)
+print(random.choice(ldata), conts)
 
 with open("README.md", "w") as f:
     f.write(f"{title}\n{conts}")
