d4663f1a99cc673f90adb7f4a90cb492e1b73dda
ivan <1440420407@qq.com>
2020-07-16 02:33:35 +0800

    update code 20200716023335

--- update.py
+++ update.py
@@ -1,14 +1,19 @@
 import json
 import random
-with open(f"poetry{random.randint(0,2)}.json", "r", encoding="utf-8") as f:
+with open(f"poetry{random.randint(0,5)}.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 title = "# བཀྲ་ཤིས་བདེ་ལེགས་"
 ldata = [jdata for idata in data["content"] for jdata in idata["content"]]
-rdata = lambda d: f"> {d['chapter']}\n> \n> {d['author']}\n> \n> " + "\n> \n> ".join(d['paragraphs']) + "\n>"
-cdata = random.choice(ldata)
-conts = rdata(cdata)
-print(cdata, conts)
+def rdata(d):
+    if 'paragraphs' not in d:
+        return f"> {d['chapter']}\n> \n> " + "\n> \n> ".join(d['content']) + "\n>"
+    elif 'author' not in d:
+        return f"> {d['chapter']}\n> \n> " + "\n> \n> ".join(d['paragraphs']) + "\n>"
+    else:
+        return f"> {d['chapter']}\n> \n> {d['author']}\n> \n> " + "\n> \n> ".join(d['paragraphs']) + "\n>"
+conts = rdata(random.choice(ldata))
+print(conts)
 
 with open("README.md", "w") as f:
     f.write(f"{title}\n{conts}")
