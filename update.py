4ffed39e26e2b06f9d9efb5b0f223a7d6cff087d
ivan <1440420407@qq.com>
2020-07-16 02:18:18 +0800

    update code 20200716021818

--- update.py
+++ update.py
@@ -6,8 +6,9 @@ with open(f"poetry{random.randint(0,2)}.json", "r", encoding="utf-8") as f:
 title = "# བཀྲ་ཤིས་བདེ་ལེགས་"
 ldata = [jdata for idata in data["content"] for jdata in idata["content"]]
 rdata = lambda d: f"> {d['chapter']}\n> \n> {d['author']}\n> \n> " + "\n> \n> ".join(d['paragraphs']) + "\n>"
-conts = rdata(random.choice(ldata))
-print(random.choice(ldata), conts)
+cdata = random.choice(ldata)
+conts = rdata(cdata)
+print(cdata, conts)
 
 with open("README.md", "w") as f:
     f.write(f"{title}\n{conts}")
