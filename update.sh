9d21241ba161bf7448d997ae51d848990537e814
ivan <1440420407@qq.com>
2020-11-19 09:29:28 +0800

    update code 20201119092928

--- update.sh
+++ update.sh
@@ -1,4 +1,2 @@
-source /Users/ivan/Desktop/ALL/Soft/python3/bin/activate
-cd /Users/ivan/Desktop/ALL/Code/PyProduct/GitHubI/IvanaXu
-python update.py
+/Users/ivan/Desktop/ALL/Soft/python3/bin/python update.py
 git add *;git commit -m "update code $(date "+%Y%m%d%H%M%S")";git push;
