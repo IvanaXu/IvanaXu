562f48928b70dd0bebd11c5113d616a028917aea
ivan <1440420407@qq.com>
2020-11-23 14:05:07 +0800

    update code 20201123140507

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ with open(f"poetry{random.randint(0,5)}.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 stats = "[![IvanaXu's github stats](https://github-readme-stats.vercel.app/api?username=IvanaXu&show_icons=true&theme=vue-dark)](https://github.com/anuraghazra/github-readme-stats)"
-toplangs = """<img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&&langs_count=3theme=vue-dark" />"""
+toplangs = """<img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&langs_count=3&theme=vue-dark" />"""
 title = f"{stats}\n{toplangs}\n# བཀྲ་ཤིས་བདེ་ལེགས་"
 ldata = [jdata for idata in data["content"] for jdata in idata["content"]]
 def rdata(d):
