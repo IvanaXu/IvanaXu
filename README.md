2f262cfd8acbad47a19f14bab87aebebaf73779e
ivan <1440420407@qq.com>
2020-11-23 14:15:46 +0800

    update code 20201123141546

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 [![IvanaXu's github stats](https://github-readme-stats.vercel.app/api?username=IvanaXu&show_icons=true&theme=vue-dark)](https://github.com/anuraghazra/github-readme-stats)
-<img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&layout=compact&theme=graywhite" />
+<img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&langs_count=3&theme=graywhite" />
 # བཀྲ་ཤིས་བདེ་ལེགས་
-> 飲湖上初晴後雨
+> 送友人
 > 
-> （宋）蘇軾
+> 李白
 > 
-> 水光瀲灩晴方好，山色空濛雨亦奇。
+> 青山橫北郭，白水繞東城。
 > 
-> 欲把西湖比西子，淡妝濃抹總相宜。
+> 此地一爲別，孤蓬萬里徵。
+> 
+> 浮雲遊子意，落日故人情。
+> 
+> 揮手自茲去，蕭蕭班馬鳴。
 >
--- update.py
+++ update.py
@@ -5,7 +5,7 @@ with open(f"poetry{random.randint(0,5)}.json", "r", encoding="utf-8") as f:
 
 # vue-dark, graywhite
 stats = "[![IvanaXu's github stats](https://github-readme-stats.vercel.app/api?username=IvanaXu&show_icons=true&theme=vue-dark)](https://github.com/anuraghazra/github-readme-stats)"
-toplangs = """<img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&layout=compact&theme=graywhite" />"""
+toplangs = """<img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&langs_count=3&theme=graywhite" />"""
 title = f"{stats}\n{toplangs}\n# བཀྲ་ཤིས་བདེ་ལེགས་"
 ldata = [jdata for idata in data["content"] for jdata in idata["content"]]
 def rdata(d):
@@ -21,3 +21,4 @@ print(conts)
 with open("README.md", "w") as f:
     f.write(f"{title}\n{conts}")
 """Thx https://github.com/chinese-poetry/chinese-poetry."""
+
