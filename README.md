c0c63f6ca89253d3d315699fa5ff483c3abe8de4
ivan <1440420407@qq.com>
2020-11-23 14:14:01 +0800

    update code 20201123141401

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-[![IvanaXu's github stats](https://github-readme-stats.vercel.app/api?username=IvanaXu&show_icons=true&theme=graywhite)](https://github.com/anuraghazra/github-readme-stats)
+[![IvanaXu's github stats](https://github-readme-stats.vercel.app/api?username=IvanaXu&show_icons=true&theme=vue-dark)](https://github.com/anuraghazra/github-readme-stats)
 <img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&langs_count=3&theme=graywhite" />
 # བཀྲ་ཤིས་བདེ་ལེགས་
-> 十二 文
+> 客中初夏
 > 
-> 家對國，武對文，四輔對三軍。九經對三史，菊馥對蘭芬。歌北鄙，詠南薰，邇聽對遙聞。召公周太保，李廣漢將軍。聞化蜀民皆草偃，爭權晉土已瓜分。巫峽夜深，猿嘯苦哀巴地月；衡峯秋早，雁飛高貼楚天雲。
+> （宋）司馬光
 > 
-> 欹對正，見對聞，偃武對修文。羊車對鶴駕，朝旭對晚曛。花有豔，竹成文，馬燧對羊欣。山中樑宰相，樹下漢將軍。施帳解圍嘉道韞，當壚沽酒嘆文君。好景有期，北嶺幾枝梅似雪；豐年先兆，西郊千頃稼如雲。
+> 四月清和雨乍晴，南山當戶轉分明。
 > 
-> 堯對舜，夏對殷，蔡惠對劉蕡。山明對水秀，五典對三墳。唐李杜，晉機雲，事父對忠君。雨晴鳩喚婦，霜冷雁呼羣。酒量洪深周僕射，詩才俊逸鮑參軍。鳥翼長隨，鳳兮洵衆禽長；狐威不假，虎也真百獸尊。
+> 更無柳絮因風起，惟有葵花向日傾。
 >
--- update.py
+++ update.py
@@ -4,8 +4,7 @@ with open(f"poetry{random.randint(0,5)}.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 # vue-dark, graywhite
-stats = "[![IvanaXu's github stats](https://github-readme-stats.vercel.app/api?username=IvanaXu&show_icons=true&theme=graywhite)](https://github.com/anuraghazra/github-readme-stats)"
-# toplangs = """[![IvanaXu's github stats](https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&langs_count=2&theme=graywhite)](https://github.com/anuraghazra/github-readme-stats)"""
+stats = "[![IvanaXu's github stats](https://github-readme-stats.vercel.app/api?username=IvanaXu&show_icons=true&theme=vue-dark)](https://github.com/anuraghazra/github-readme-stats)"
 toplangs = """<img align="right" src="https://github-readme-stats.vercel.app/api/top-langs/?username=IvanaXu&langs_count=3&theme=graywhite" />"""
 title = f"{stats}\n{toplangs}\n# བཀྲ་ཤིས་བདེ་ལེགས་"
 ldata = [jdata for idata in data["content"] for jdata in idata["content"]]
