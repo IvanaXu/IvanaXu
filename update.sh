33db6589c860c2b7e08c9aab3db9652aff96faa5
ivan <ivan@example.com>
2022-08-25 16:22:12 +0800

    update code 20220825162212

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 # cd /Users/ivan/Documents/GitHub/IvanaXu
 python3 update.py
-# git add *;git commit -m "update code $(date "+%Y%m%d%H%M%S")";git push;
+
+git add *;git commit -m "update code $(date "+%Y%m%d%H%M%S")";git push;
