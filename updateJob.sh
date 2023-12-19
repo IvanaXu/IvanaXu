588fe7e68fe41453e90a60abf2f16c14f884ac5f
IvanaXu <1440420407@qq.com>
2023-12-19 22:02:21 +0800

    Auto@Mac

--- updateJob.sh
+++ updateJob.sh
@@ -6,5 +6,5 @@ git pull
 /Users/ivan/opt/anaconda3/bin/python update.py;echo;
 
 git add *;echo;
-git commit -m "MACmini $(date "+%Y%m%d%H%M%S"|md5sum)";echo;
+git commit -m "Auto@Mac";echo;
 git push;
