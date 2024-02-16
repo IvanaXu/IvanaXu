08233af053f0f46f8769a1f05ac02be6e299257f
IvanaXu <1440420407@qq.com>
2024-02-17 00:39:19 +0800

    Auto@Mac #1 00

--- update.py
+++ update.py
@@ -42,13 +42,10 @@ monkeytype = f"""{t0}&up_color=yellow&up_message=Monkeytype&url={t1})](https://m
 # latest
 # title = f"""{stats}\n{toplangs}\n{wakatime}\n# བཀྲ་ཤིས་བདེ་ལེགས་\t{wakatime_total}\t{tianchi}\t{yuque}\t{leetcode}\t{aistduio}\t{gitee}"""
 # title = f"""{stats}\n# བཀྲ་ཤིས་བདེ་ལེགས་\t{wakatime_total}\t{tianchi}\t{yuque}\t{leetcode}\t{aistduio}\t{gitee}"""
-
 title = (
-    f"""{wakatime} \n"""
     f"""- བཀྲ་ཤིས་བདེ་ལེགས་ \n"""
     f"""- {wakatime_total}\t{tianchi}\t{kaggle}\t{yuque}\t{leetcode}\t{aistduio}\t{gitee}\t{monkeytype} \n"""
 )
-
 
 ## 
 if random.random() < 0.618:
