0e300d038c749584179917a2d7bd9da4bc634fd3
IvanaXu <1440420407@qq.com>
2024-02-20 16:14:24 +0800

    Auto@Mac #1 16

--- update.py
+++ update.py
@@ -29,7 +29,7 @@ stats = f"[![IvanaXu's github stats]({url1})]({url2})"
 toplangs = f"""<img align="right" src="{url3}" />"""
 wakatime = f"""<img src="{url4}" />"""
 wakatime_total = f"""[![wakatime](https://wakatime.com/badge/user/5043ee4a-e361-4607-9d47-d557f2005d05.svg)](https://wakatime.com/@5043ee4a-e361-4607-9d47-d557f2005d05)"""
-wakatime_figure = f"""<figure><embed src="https://wakatime.com/share/@IvanAXu/a3a1609c-6d66-4f56-a392-9d770eeec1ec.svg"></embed></figure>"""
+wakatime_figure = f"""<a href="https://wakatime.com"><img src="https://wakatime.com/share/@IvanAXu/06501b1d-f434-4f2a-9524-dc2196223971.png" /></a>"""
 rtitle = f"""<img align="right" src="{url4}" />"""
 
 t0 = "[![Website](https://img.shields.io/website?label="
